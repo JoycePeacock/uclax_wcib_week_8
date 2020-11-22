a98c38d5fb3297350044e81981a73254a273574d
Joyce <jpeacock@cypresscollege.edu>
2020-11-22 14:34:39 -0800

    Some css and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 jQuery(function() {
-	
+
+	$('#button_image').hover(function () {
+	        $('#button_image').attr('src', 'images/btn_apple_download_hover.png');
+				},
+				function () {
+				 $('#button_image').attr('src', 'images/btn_apple_download.png');
+	});
+
 });
